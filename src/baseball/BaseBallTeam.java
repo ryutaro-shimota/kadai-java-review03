9e23d6746d2457be25d09a7c5697ae26814239d8
ryutaro-shimota <ryutaro.2000.radiology@gmail.com>
2023-02-04 18:52:13 +0900

    add BaseballTeam.java add Review03.java

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,61 @@
+package baseball;
+
+public class BaseBallTeam {
+
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    public BaseBallTeam() {
+    }
+
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public double getRate() {
+        double rate = (double)( this.win / (double)(this.win + this.lose)) ;
+        return rate;
+    }
+
+    public void report() {
+        System.out.println(this.name + "の2022年度の成績は " + this.win + "勝 " + this.lose + "敗 " + this.draw + "分、勝率は " + getRate() + "です。");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+     public void setLose(int lose) {
+        this.lose = lose;
+     }
+
+     public int getDraw() {
+         return draw;
+     }
+
+      public void setDraw(int draw) {
+         this.draw = draw;
+      }
+
+}
